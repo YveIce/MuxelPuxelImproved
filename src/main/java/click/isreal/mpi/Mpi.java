c5def9d984c862d3add166869847f780b1133521
YveIce <42624289+YveIce@users.noreply.github.com>
2023-10-13 02:22:12 +0200

    add direct play button on title screen

--- src/main/java/click/isreal/mpi/Mpi.java
+++ src/main/java/click/isreal/mpi/Mpi.java
@@ -24,14 +24,23 @@
 
 package click.isreal.mpi;
 
-import click.isreal.mpi.client.mpiClient;
 import click.isreal.mpi.config.Config;
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.Level;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.network.ServerAddress;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FileWriter;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
@@ -41,16 +50,11 @@ public class Mpi implements ModInitializer
 
   public static final Logger LOGGER = LogManager.getFormatterLogger("MPI");
   private static Mpi instance;
+  private final Config config = new Config();
   public DecimalFormat moneyformat = new DecimalFormat("###,###,###.00 ⛀", new DecimalFormatSymbols(Locale.GERMANY));
-  private Config config = new Config();
 
   {
     instance = this;
-  }
-
-  public Mpi()
-  {
-    // load config here
   }
 
   public static Mpi getInstance()
@@ -68,12 +72,34 @@ public class Mpi implements ModInitializer
       System.setProperty("log4j2.formatMsgNoLookups", "true");
       LOGGER.info("Log4j2 Security Patch applied, now you are safe! ;-)");
     }
+
+    // Yve: Patch Realms Button on Start Screen
+    ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) ->
+    {
+      if (screen instanceof TitleScreen)
+      {
+        for (ClickableWidget widget : Screens.getButtons(screen))
+        {
+          if (widget.getMessage().getString().toLowerCase().contains("realms"))
+          {
+            widget.visible = false;
+          }
+        }
+        Screens.getButtons(screen).add(
+            ButtonWidget.builder(
+                    Text.literal(Formatting.GOLD + "Play MixelPixel"),
+                    button -> this.joinMixel()
+                )
+                .dimensions(scaledWidth / 2 - 100, scaledHeight / 4 + 96, 200, 20)
+                .build()
+        );
+      }
+    });
   }
 
-
-
-
-
-
-
+  public void joinMixel()
+  {
+    ServerInfo mpServer = new ServerInfo("MixelPixel", "play.mixelpixel.net", false);
+    ConnectScreen.connect(new MultiplayerScreen(new TitleScreen()), MinecraftClient.getInstance(), ServerAddress.parse(mpServer.address), mpServer, false);
+  }
 }
